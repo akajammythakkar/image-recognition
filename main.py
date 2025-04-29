c43dc10cc12e02dc643613f9a8cfc89d75ef7ada
Jay Thakkar <akajammythakkar@gmail.com>
2025-04-29 23:13:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 app.secret_key = "supersecretkey"  # Needed for session storage
 
 # Configuration
-PROJECT = "YOUR_PROJECT_ID"
-ENDPOINT_ID = "VERTEXAIENDPOINT"
+PROJECT = "97482719406"
+ENDPOINT_ID = "1995891780855267328"
 LOCATION = "us-central1"
 API_ENDPOINT = "us-central1-aiplatform.googleapis.com"
 
